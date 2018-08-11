0cb23e8784e3491986bc9e62deace82e0f9522ca
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-08-11 18:44:47 +0200

    PlantUML diagrams

--- cdbookstore/src/app/app-routing.module.ts
+++ cdbookstore/src/app/app-routing.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { GeneratorComponent } from 'generator';
-import { InventoryComponent } from 'inventory';
-import { StoreComponent } from 'store';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  // {path: 'gen', component: GeneratorComponent},
-  // {path: 'inv', component: InventoryComponent},
-  // {path: 'str', component: StoreComponent},
 ];
 
 @NgModule({
